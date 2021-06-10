82f90e5775528907163042c799af835126e22a96
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2021-06-10 23:13:04 +0500

    2020/06/10 2312

---
+++ l12_textwrap_indent.py
@@ -0,0 +1,10 @@
+import textwrap
+from l8_textwrap_example import sample_text
+
+dedented_text = textwrap.dedent(sample_text)
+wrapped = textwrap.fill(dedented_text, width=50)
+wrapped += '\n\nSecond paragraph after a blank line.'
+final = textwrap.indent(wrapped, '> ')
+
+print('Quoted block:Xn')
+print(final)
