d86fb3d1e7c18092ed0359b578462d89d25b55ff
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-04-26 09:52:22 +0500

    linecache: Read Text Files Eﬃciently

---
+++ chapter6/l45_linecache_getline.py
@@ -0,0 +1,14 @@
+import linecache
+from l44_linecache_data import *
+
+filename = make_tempfile()
+
+# Pick out the same line from source and cache.
+# (Notice that linecache counts from 1.)
+print('SOURCE:')
+print('{!r}'.format(lorem.split('\n')[4]))
+print()
+print('CACHE:')
+print('{!r}'.format(linecache.getline(filename, 5)))
+
+cleanup(filename)
