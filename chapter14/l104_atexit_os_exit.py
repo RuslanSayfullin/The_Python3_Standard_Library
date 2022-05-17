c54b53c37f54bd558b523090988b7f97c3dc0fa1
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-05-17 10:33:43 +0500

    atexit: Program Shutdown Callbacks

---
+++ chapter14/l104_atexit_os_exit.py
@@ -0,0 +1,10 @@
+import atexit
+import os
+
+def not_called():
+print('This should not be called')
+print('Registering')
+atexit.register(not_called)
+print('Registered')
+print('Exiting...')
+os._exit(0)
---
+++ chapter14/l99_atexit_decorator.py
@@ -0,0 +1,8 @@
+import atexit
+
+
+@atexit.register
+def all_done():
+    print('all_done()')
+    
+print('starting main program')
