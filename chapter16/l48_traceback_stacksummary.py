c13c94812f595b1d67f290e69c1d1498d08b42c9
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-05-20 09:55:30 +0500

    traceback: Exceptions and Stack Traces

---
+++ chapter16/l48_traceback_stacksummary.py
@@ -0,0 +1,17 @@
+import traceback
+import sys
+
+from l47_traceback_example import call_function
+
+
+def f():
+    summary = traceback.StackSummary.extract(traceback.walk_stack(None))
+    print(''.join(summary.format()))
+
+
+print('Calling f() directly:')
+f()
+
+print()
+print('Calling f() from 3 levels deep:')
+call_function(f)
