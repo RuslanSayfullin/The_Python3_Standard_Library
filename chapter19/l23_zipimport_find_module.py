367608a970fd9759558c553d5a48d44f44a9f5c3
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-05-29 22:50:20 +0500

    zipimport: Load Python Code from ZIP Archives

---
+++ chapter19/l23_zipimport_find_module.py
@@ -0,0 +1,7 @@
+import zipimport
+
+importer = zipimport.zipimporter('zipimport_example.zip')
+
+for module_name in ['zipimport_find_module', 'not_there']:
+    print(module_name, ':', importer.find_module(module_name))
+    
