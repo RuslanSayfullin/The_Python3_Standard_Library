1245205c974b674febf7624f427d72e6ea37dfc9
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-05-29 22:45:15 +0500

    pkgutil: Package Utilities

---
+++ chapter19/l13_pkgutil_devel.py
@@ -0,0 +1,7 @@
+import demopkg2
+print('demopkg2:', demopkg2.__file__)
+
+import demopkg2.overloaded
+print('demopkg2.overloaded:', demopkg2.overloaded.__file__)
+print()
+demopkg2.overloaded.func()
