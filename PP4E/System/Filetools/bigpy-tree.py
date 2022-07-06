39d32ef29f31368ed0dcaff4f51bc28cca01010c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-07-06 11:42:56 +0500

    Сканирование дерева каталого стандартной библиотеки

---
+++ PP4E/System/Filetools/bigpy-tree.py
@@ -0,0 +1,27 @@
+"""
+Отыскивает наибольший файл с исходным программным кодом на языке Python в дереве
+каталогов.
+Поиск выполняется в каталоге стандартной библиотеки, отображение результатов
+выполняется с помощью модуля pprint.
+"""
+
+import sys, os, pprint
+trace = False
+if sys.platform.startswith('win'):
+    dirname = r'C:\Python31\Lib'        # Windows
+else:
+    dirname = '/usr/lib/python'         # Unix, Linux, Cygwin
+
+allsizes = []
+for (thisDir, subsHere, filesHere) in os.walk(dirname):
+    if trace: print(thisDir)
+    for filename in filesHere:
+        if filename.endswith('.py'):
+            if trace: print('...', filename)
+            fullname = os.path.join(thisDir, filename)
+            fullsize = os.path.getsize(fullname)
+            allsizes.append((fullsize, fullname))
+
+allsizes.sort()
+pprint.pprint(allsizes[:2])
+pprint.pprint(allsizes[-2:])
