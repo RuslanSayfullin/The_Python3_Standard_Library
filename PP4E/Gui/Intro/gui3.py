22e3b110115ada92ad918de1d11be3b2c5598b4c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-07-21 12:19:25 +0500

    Добавление пользовательских обработчиков

---
+++ PP4E/Gui/Intro/gui3.py
@@ -0,0 +1,12 @@
+import sys
+from tkinter import *
+
+
+def quit():                         # собственный обработчик событий
+    print('Hello, I must be going...')
+    sys.exit()                      # закрыть окно и завершить процесс
+
+    
+widget = Button(None, text='Hello event world', command=quit)
+widget.pack()
+widget.mainloop()
