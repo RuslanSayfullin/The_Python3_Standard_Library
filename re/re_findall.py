3cc32cf43698f3557b58ad946254c54df068374d
cryptolis <ruslansaifullin91@gmail.com>
2023-12-11 00:02:04 +0500

    re многократные совподения

---
+++ re/re_findall.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+#
+#
+"""Repetition of patterns
+"""
+
+#end_pymotw_header
+import re
+
+text = 'abbaaabbbbaaaaa'
+
+pattern = 'ab'
+
+for match in re.findall(pattern, text):
+    print('Found {!r}'.format(match))
---
+++ re/re_finditer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+#
+#
+"""Repetition of patterns
+"""
+
+#end_pymotw_header
+import re
+
+text = 'abbaaabbbbaaaaa'
+
+pattern = 'ab'
+
+for match in re.finditer(pattern, text):
+    s = match.start()
+    e = match.end()
+    print('Found {!r} at {:d}:{:d}'.format(
+        text[s:e], s, e))
