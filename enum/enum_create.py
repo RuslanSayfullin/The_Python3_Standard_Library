a3599820615cdd9bf0d100dd9067951c831d79cd
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-03-09 23:13:33 +0500

    enum. Создание перечислений

--- enum/enum_create.py
+++ enum/enum_create.py
@@ -1,14 +1,7 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-"""
-"""
-
-#end_pymotw_header
 import enum
 
-
 class BugStatus(enum.Enum):
-    """Создание перечислении."""
+
     new = 7
     incomplete = 6
     invalid = 5
@@ -16,7 +9,6 @@ class BugStatus(enum.Enum):
     in_progress = 3
     fix_committed = 2
     fix_released = 1
-
 
 print('\nMember name: {}'.format(BugStatus.wont_fix.name))
 print('Member value: {}'.format(BugStatus.wont_fix.value))
