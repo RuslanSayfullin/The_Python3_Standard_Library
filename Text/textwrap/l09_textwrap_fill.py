53b3a5f7310f54715e03000759bfca2d67234b7c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-05-01 22:52:40 +0500

    модуль textwrap

---
+++ Text/textwrap/l09_textwrap_fill.py
@@ -0,0 +1,4 @@
+import textwrap
+from l08_textwrap_example import sample_text
+
+print(textwrap.fill(sample_text, width=50))
---
+++ Text/textwrap/l10_textwrap_dedent.py
@@ -0,0 +1,6 @@
+import textwrap
+from l08_textwrap_example import sample_text
+
+dedented_text = textwrap.dedent(sample_text)
+print("Dedented: ")
+print(dedented_text)
