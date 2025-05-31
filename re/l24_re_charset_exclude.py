cbfa76f7cb7dbcb2343afebcad95b267894ca9f5
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-05-31 22:42:33 +0500

    Наборы символов

---
+++ re/l24_re_charset_exclude.py
@@ -0,0 +1,6 @@
+from l20_re_test_patterns import test_patterns
+
+test_patterns(
+    'This is some text -- with punctuation.',
+    [('[^--. ]+', 'sequences without -, ., or space')],
+    )
---
+++ re/l25_re_charset_ranges.py
@@ -0,0 +1,9 @@
+from l20_re_test_patterns import test_patterns
+
+test_patterns(
+    'This is some test -- with punctuation.',
+    [('[a-z]+', 'sequences of lowercase letters'),
+    ('[A-Z]+', 'sequences of uppercase letters'),
+    ('[a-azA-Z]+', 'sequences of lower - or uppercase  letters'),
+    ('[A-Z] [a-z]+', 'one uppercase followed by lowercase')],
+)
